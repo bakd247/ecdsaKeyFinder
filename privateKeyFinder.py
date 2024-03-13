e7963c312ab224618a9045b02f49b958184afc2d
Robert Gordon <rjgcustoms@hotmail.com>
2024-03-12 20:46:42 -0400

    Update privateKeyFinder.py
    Changed From Linear Search to Binary Search resulting in a  100% speed increase for each round of random keys generated

--- privateKeyFinder.py
+++ privateKeyFinder.py
@@ -1,5 +1,8 @@
 from tinyec.ec import SubGroup, Curve
 from os import urandom
+from binarySearch import binarySearch
+from byteAdderOptimized import createGrid, multiplyNum
+from tqdm import tqdm, trange
 import time
 X = int((input("Please Enter Your Public Key X Coordinate In Hexidecimal Format:")),16)
 Y = int((input("Please Enter Your Public Key Y Coordinate In Hexidecimal Format:")),16)
@@ -14,62 +17,63 @@ if (((X * X * X) % p) + 7) != (Y * Y) % p:
 	print("The Public Key X and Y Coordinates You Entered Are NOT Valid...NOTE: DO NOT Include 02, 03 Or 04 At The Begining Of The X Coordinate And Make Sure You Are Using Hexidecimal Format. If You Need Assistance...Please Contact Technical Support...Please Try Again...Enter Valid X and Y Coordinates For Your Public Key...")
 	exit()
 else:
-	curve1 = Curve(a, b, SubGroup(p, g, n, h), name)
-	pubKeyHalves = curve1.g * 1  
+	curve1 = Curve(a, b, SubGroup(p, g, n, h), name) 
 	EnteredPublicKey = curve1.g * 1
 	CollisionList = []
+	PosOneList = []
 	N = 115792089237316195423570985008687907852837564279074904382605163141518161494337
 	half = 57896044618658097711785492504343953926418782139537452191302581570759080747169
 	AA = int(input("Please Enter the Size of the Collision List you would like to Create. Best Performance around 10,000:"))
 	AAA = (AA*2)
+	BB = AA
 	print("Creating Collision List...Please Wait...")
-	place = pubKeyHalves * ((half ** AA)%N)
-	CollisionList.append(place.x)
-	for iterationMultiples in range (AAA):
-		iterationMultiple = place + place
-		CollisionList.append(iterationMultiple.x)
-		place = iterationMultiple
-	CollisionList.reverse	
-	tupleCollisionList = tuple(CollisionList)
-	print("Collision List Created...")
-	print("Creating Lookup Table...")
-	from byteAdderOptimized import multiplyNum
-	print("Lookup table Created...Searching for Your Key...Please Wait....")
-	print("Total Key Comparisons per Round:", AAA)
-	KeyFound = False
-	while KeyFound != True:
-		t = time.process_time()
-		privKey = (int((((urandom(32))[2:])).hex(), 16))%N		##Change this value for testing from a random 256 bit hash to a known private Key Integer...
-		privateKey1 = (privKey * (half ** AA))%N				##Then Enter the Public key or Public key of a multiple
-		keyB = multiplyNum(privateKey1)
-		for hashIteration in range(AAA):
-			if keyB.x in tupleCollisionList:
-				for item, key in enumerate (tupleCollisionList):
-					if key != keyB.x:
-							pass
-					else:
-						print("Key Found")
-						privateKey = ((privateKey1 * (hashIteration + 1))*(2**(AA - item)))%N
-						keyC = multiplyNum(privateKey)
-						if keyC.y != EnteredPublicKey.y:
-							privateKey = n - privateKey
-						else:
-							privateKey = privateKey
-						print("Hash Iteration:", hashIteration)
-						print("Multiple Iteration:", (AA-item))
-						print("Collision Public Key:", keyB.x)
-						print("Collision Private Key:", privateKey1)
-						print("Public Key Your Entered", EnteredPublicKey)
-						print("Actual Private Key:", privateKey)
-						print("Please Do Not Loose This Key...Thank You")
-						print("This Key Has Been Written To A File Called foundKeys.txt")
-						with open('foundKeys.txt', 'w') as e:
-							e.write(str(privateKey))
-						KeyFound = True
-						exit()
+	place = EnteredPublicKey * ((half ** AA)%N)
+	PosOneList.append(place.x)
+	PosOneList.append(-AA)
+	tuplePosOneList = tuple(PosOneList)
+	CollisionList.append(tuplePosOneList)
+	AA = -AA + 1
+	for iterationMultiples in trange (AAA,total=AAA,ascii=True,ncols=100,colour='#00ff00',unit='Keys Stored',desc='Keys Stored In Memory...'):
+		PosTwoList = []
+		iteratedMultiple = place + place
+		PosTwoList.append(iteratedMultiple.x)
+		PosTwoList.append(AA)
+		tuplePosTwoList = tuple(PosTwoList)
+		CollisionList.append(tuplePosTwoList)
+		place = iteratedMultiple
+		AA += 1
+print("SortingList...Please Wait...")
+CollisionList.sort(key = lambda i:i[0])
+tupleCollisionList = tuple(CollisionList)
+print("List Sorted...Searching For Key")
+keyFound = False
+while keyFound != True:
+	t = time.process_time()
+	privKey = (int((((urandom(32))[2:])).hex(), 16))%N
+	privateKey1 = (privKey * (half ** BB))%N
+	newKey = multiplyNum(privateKey1)
+
+##Insert EasyCount Here....
+
+	for hashIteration in trange (AAA,total=AAA,ascii=True,ncols=100,colour='#00ff00',unit='Comparisons',desc='Searching...'):
+		keyToFind = int(newKey.x)
+		result = binarySearch(tupleCollisionList, keyToFind)
+		if result != -1:
+			if result[1] <= 0:
+				RecFunct = ((privateKey1 * (2 ** hashIteration)) * (2 ** (abs(result[1]))))%N
 			else:
-				keyB += keyB
-		elapsed_time = time.process_time() - t
-		print("Average Key Strings Compared Per Second",(AAA//elapsed_time))
-		print("Average Seconds per Round ", elapsed_time)
-		KeyFound = False
+				RecFunct = ((privateKey1 * (half ** hashIteration)) * (2 ** (abs(result[1]))))%N
+			recoveredKey = multiplyNum(RecFunct)
+			if recoveredKey.y != EnteredPublicKey.y:
+				RecFunct = N - RecFunct
+			print("X-Coordinate Found of Collision Key Found:", result)
+			print("Collision PrivateKey:", privateKey1)
+			print("Here is the Private Key for the PublicKey that you Entered:", RecFunct)
+			keyFound = True
+			exit()
+		else:
+			newKey += newKey
+
+	elapsed_time = time.process_time() - t
+	print("Average Random Key Strings Created Per Second",(AAA//elapsed_time))
+	print("Average Seconds per Round ", elapsed_time)
